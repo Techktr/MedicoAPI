fdb6eb185a2ac3b197d10726c047c6f7a58b9e79
kevin-trochon <157088235+Techktr@users.noreply.github.com>
2026-02-28 17:45:06 +1100

    feat: ajout de la couche service.

---
+++ Services/IPatientService.cs
@@ -0,0 +1,12 @@
+﻿using MedicoAPI.Services.DTOs;
+
+namespace MedicoAPI.Services;
+
+public interface IPatientService
+{
+    List<PatientResponseDto> GetPatients();
+    PatientResponseDto GetPatientById(int id);
+    PatientResponseDto CreatePatient(PatientCreateDto patientCreateDto);
+    PatientResponseDto UpdatePatient(PatientUpdateDto patientUpdateDto);
+    void DeletePatient(PatientDeleteDto patientDeleteDto);
+}
---
+++ Services/PatientService.cs
@@ -0,0 +1,61 @@
+﻿using MedicoAPI.Models;
+using MedicoAPI.Repositories;
+using MedicoAPI.Services.DTOs;
+using MedicoAPI.Services.Mappers;
+
+namespace MedicoAPI.Services;
+
+public class PatientService: IPatientService
+{
+
+    private IPatientRepository _patientRepository;
+    private PatientMapper _patientMapper;
+
+    public PatientService(IPatientRepository patientRepository, PatientMapper patientMapper)
+    {
+        _patientRepository = patientRepository;
+        _patientMapper = patientMapper;
+    }
+
+    public List<PatientResponseDto> GetPatients()
+    {
+        return _patientMapper.EntityToResponseDtos(_patientRepository.GetPatients());
+    }
+
+    public PatientResponseDto GetPatientById(int id)
+    {
+        Patient? patient = _patientRepository.GetPatientById(id);
+        if (patient == null)
+        {
+            throw new Exception("Patient not found");
+        }
+        PatientResponseDto patientResponse = _patientMapper.EntityToResponseDto(patient);
+        return patientResponse ?? throw new Exception("Patient not found");
+    }
+
+    public PatientResponseDto CreatePatient(PatientCreateDto patientCreateDto)
+    {
+        Patient patient = _patientRepository.CreatePatient(_patientMapper.CreateDtoToEntity(patientCreateDto));
+        if (patient == null)
+        {
+            throw new Exception("Patient not found");
+        }
+        return _patientMapper.EntityToResponseDto(patient);
+    }
+
+    public PatientResponseDto UpdatePatient(PatientUpdateDto patientUpdateDto)
+    {
+        Patient? patient = _patientRepository.UpdatePatient(_patientMapper.UpdateDtoToEntity(patientUpdateDto));
+        if (patient == null)
+        {
+            throw new Exception("Patient not found");
+        }
+        PatientResponseDto patientResponse = _patientMapper.EntityToResponseDto(patient);
+        return patientResponse ?? throw new Exception("Patient not found");
+    }
+
+    public void DeletePatient(PatientDeleteDto patientDeleteDto)
+    {
+        _patientRepository.DeletePatient(_patientMapper.DeleteDtoToEntity(patientDeleteDto));
+    }
+}
