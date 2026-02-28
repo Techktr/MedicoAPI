f0c63274742c02b54502c27fe1008a77aaaa28b5
kevin-trochon <157088235+Techktr@users.noreply.github.com>
2026-02-28 17:18:48 +1100

    feat: ajouts du mapper des Dtos vers Entity et Entity vers Dtos.

---
+++ Services/Mappers/PatientMapper.cs
@@ -0,0 +1,69 @@
+﻿using MedicoAPI.Models;
+using MedicoAPI.Services.DTOs;
+
+namespace MedicoAPI.Services.Mappers;
+
+public class PatientMapper
+{
+
+    public PatientResponseDto EntityToResponseDto(Patient patient)
+    {
+        return new PatientResponseDto
+        {
+
+            Id = patient.Id,
+            FirstName = patient.FirstName,
+            LastName = patient.LastName,
+            Age = patient.Age,
+            Address = patient.Address,
+            SportName = patient.SportName
+        };
+    }
+
+    public List<PatientResponseDto> EntityToResponseDtos(List<Patient> patients)
+    {
+        return patients.Select(patient => EntityToResponseDto(patient)).ToList();
+    }
+
+    public Patient ResponseDtoToEntity(PatientResponseDto dto)
+    {
+        return new Patient()
+        {
+            Id = dto.Id,
+            FirstName = dto.FirstName,
+            LastName = dto.LastName,
+            Age = dto.Age,
+            Address = dto.Address,
+            SportName = dto.SportName
+        };
+    }
+
+    public Patient CreateDtoToEntity(PatientCreateDto dto)
+    {
+        return new Patient()
+        {
+            FirstName = dto.FirstName,
+            LastName = dto.LastName,
+            Age = dto.Age,
+            Address = dto.Address,
+            SportName = dto.SportName
+        };
+    }
+
+    public Patient UpdateDtoToEntity(PatientUpdateDto dto)
+    {
+        return new Patient()
+        {
+            Id = dto.Id,
+            Address = dto.Address,
+            Age = dto.Age,
+            SportName = dto.SportName
+        };
+    }
+
+    public Patient DeleteDtoToEntity(PatientDeleteDto dto)
+    {
+        return new Patient(){Id = dto.Id};
+    }
+
+}
