50ca6ee02554c0b7e33a30ca6695f411c37de311
kevin-trochon <157088235+Techktr@users.noreply.github.com>
2026-03-08 17:48:05 +1100

    fix: correction sérialisation JSON camelCase.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using System.Text.Json;
 using MedicoAPI.Data;
 using MedicoAPI.Repositories;
 using MedicoAPI.Services;
@@ -8,7 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(options => options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
