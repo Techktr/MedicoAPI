17513ea7dad46dfb1e7d94899aee06a96e774740
kevin-trochon <157088235+Techktr@users.noreply.github.com>
2026-03-29 15:44:58 +1100

    feat: migration de Appointment par EF Core.

--- Migrations/20260329044345_AppointmentTable.Designer.cs
+++ Migrations/20260329044345_AppointmentTable.Designer.cs
@@ -4,6 +4,7 @@ using MedicoAPI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MedicoAPI.Migrations
 {
     [DbContext(typeof(MedicoDbContext))]
-    partial class MedicoDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260329044345_AppointmentTable")]
+    partial class AppointmentTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
