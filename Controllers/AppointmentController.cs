6f68aa0f66ee6606dcab3bb50e29e809ba25151c
kevin-trochon <157088235+Techktr@users.noreply.github.com>
2026-03-29 15:42:01 +1100

    feat: ajout du controller pour les rendez-vous.

---
+++ Controllers/AppointmentController.cs
@@ -0,0 +1,50 @@
+﻿using MedicoAPI.Models;
+using MedicoAPI.Services;
+using MedicoAPI.Services.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MedicoAPI.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AppointmentController: ControllerBase
+{
+    private IAppointmentService _service;
+
+    public AppointmentController(IAppointmentService service)
+    {
+        _service = service;
+    }
+
+    [HttpGet]
+    public ActionResult<List<AppointmentResponseDto>> GetAppointments()
+    {
+        return Ok(_service.GetAppointments());
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<AppointmentResponseDto> GetAppointmentById(int id)
+    {
+        return Ok(_service.GetAppointmentById(id));
+    }
+
+    [HttpPost]
+    public ActionResult<AppointmentResponseDto> CreateAppointment(AppointmentCreateDto appointmentCreateDto)
+    {
+        AppointmentResponseDto appointmentResponseDto =  _service.CreateAppointment(appointmentCreateDto);
+        return Created($"api/patient/{appointmentResponseDto.Id}", appointmentResponseDto);
+    }
+
+    [HttpPut("{id}")]
+    public ActionResult<AppointmentResponseDto> UpdateAppointment(AppointmentUpdateDto appointmentUpdateDto)
+    {
+        return Ok(_service.UpdateAppointment(appointmentUpdateDto));
+    }
+
+    [HttpDelete("{id}")]
+    public ActionResult<AppointmentResponseDto> DeleteAppointment(int id)
+    {
+        _service.DeleteAppointment(id);
+        return NoContent();
+    }
+}
