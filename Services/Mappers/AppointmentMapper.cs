b7943929b96bd1aedb8fb23f2f49d2159173b6fc
kevin-trochon <157088235+Techktr@users.noreply.github.com>
2026-03-29 14:59:54 +1100

    feat: ajouts du mapper entity vers dto.

---
+++ Services/Mappers/AppointmentMapper.cs
@@ -0,0 +1,45 @@
+﻿using MedicoAPI.Models;
+using MedicoAPI.Services.DTOs;
+
+namespace MedicoAPI.Services.Mappers;
+
+public class AppointmentMapper
+{
+    public AppointmentResponseDto EntityToResponseDto(Appointment entity)
+    {
+        return new AppointmentResponseDto()
+        {
+            Id = entity.Id,
+            Date = entity.Date,
+            PatientId = entity.PatientId,
+            PatientFullName = entity.Patient.LastName + " " + entity.Patient.FirstName,
+            PractitionerId = entity.PractitionerId,
+            PractitionerFullName =  entity.Practitioner.LastName + " " + entity.Practitioner.FirstName,
+        };
+    }
+
+    public List<AppointmentResponseDto> EntityToResponseDtos(List<Appointment> entities){
+        return entities.Select(entity => EntityToResponseDto(entity)).ToList();
+    }
+
+    public Appointment EntityToCreateDto(AppointmentCreateDto dto)
+    {
+        return new Appointment()
+        {
+            Date = dto.Date,
+            PatientId = dto.PatientId,
+            PractitionerId = dto.PractitionerId,
+        };
+    }
+
+    public Appointment EntityToUpdateDto(AppointmentUpdateDto dto)
+    {
+        return new Appointment()
+        {
+            Id = dto.Id,
+            Date = dto.Date,
+            PatientId = dto.PatientId,
+            PractitionerId = dto.PractitionerId
+        };
+    }
+}
