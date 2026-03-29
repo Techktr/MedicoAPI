e172d4114bc6d29be96e347942e8c34547fd8de5
kevin-trochon <157088235+Techktr@users.noreply.github.com>
2026-03-29 12:53:14 +1100

    feat: ajout du services de practitioner.

--- Services/IPractitionerService.cs
+++ Services/IPractitionerService.cs
@@ -1,6 +1,12 @@
-﻿namespace MedicoAPI.Services;
+﻿using MedicoAPI.Services.DTOs;
+
+namespace MedicoAPI.Services;
 
 public interface IPractitionerService
 {
-    
+    List<PractitionerResponseDto> GetPractitioners();
+    PractitionerResponseDto GetPractitionerById(int id);
+    PractitionerResponseDto CreatePractitioner(PractitionerCreateDto practitionerCreateDto);
+    PractitionerResponseDto UpdatePractitioner(PractitionerUpdateDto practitionerUpdateDto);
+    void DeletePractitioner(int id);
 }
--- Services/PractitionerService.cs
+++ Services/PractitionerService.cs
@@ -1,6 +1,60 @@
-﻿namespace MedicoAPI.Services;
+﻿using MedicoAPI.Models;
+using MedicoAPI.Repositories;
+using MedicoAPI.Services.DTOs;
+using MedicoAPI.Services.Mappers;
 
-public class PractitionerService
+namespace MedicoAPI.Services;
+
+public class PractitionerService: IPractitionerService
 {
-    
+    private IPractitionerRepository _practitionerRepository;
+    private PractitionerMapper _practitionerMapper;
+
+    public PractitionerService(IPractitionerRepository practitionerRepository, PractitionerMapper practitionerMapper)
+    {
+        _practitionerRepository = practitionerRepository;
+        _practitionerMapper = practitionerMapper;
+    }
+
+    public List<PractitionerResponseDto> GetPractitioners()
+    {
+        return _practitionerMapper.EntityToResponseDtos(_practitionerRepository.GetPractitioners());
+    }
+
+    public PractitionerResponseDto GetPractitionerById(int id)
+    {
+       Practitioner? practitioner = _practitionerRepository.GetPractitionerById(id);
+       if (practitioner == null)
+       {
+           throw new Exception("Practitioner not found");
+       }
+
+       PractitionerResponseDto response = _practitionerMapper.EntityToResponseDto(practitioner);
+       return response ?? throw new Exception("Practitioner not found");
+    }
+
+    public PractitionerResponseDto CreatePractitioner(PractitionerCreateDto practitionerCreateDto)
+    {
+        Practitioner practitioner = _practitionerRepository.createPractitioner(_practitionerMapper.EntityToCreateDto(practitionerCreateDto));
+        if (practitioner == null)
+        {
+            throw new Exception("Practitioner not found");
+        }
+        return _practitionerMapper.EntityToResponseDto(practitioner);
+    }
+
+    public PractitionerResponseDto UpdatePractitioner(PractitionerUpdateDto practitionerUpdateDto)
+    {
+        Practitioner? practitioner = _practitionerRepository.updatePractitioner(_practitionerMapper.EntityToUpdateDto(practitionerUpdateDto));
+        if (practitioner == null)
+        {
+            throw new Exception("Practitioner not found");
+        }
+        return _practitionerMapper.EntityToResponseDto(practitioner) ?? throw new Exception("Patient not found");
+    }
+
+    public void DeletePractitioner(int id)
+    {
+        _practitionerRepository.DeletePractitioner(id);
+    }
 }
