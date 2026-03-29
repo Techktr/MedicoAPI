0fd6e5ce42313227e515c125ab58c7cbcb762728
kevin-trochon <157088235+Techktr@users.noreply.github.com>
2026-03-29 14:43:04 +1100

    feat: ajout du modele des rendez-vous.

---
+++ Models/Appointment.cs
@@ -0,0 +1,11 @@
+﻿namespace MedicoAPI.Models;
+
+public class Appointment
+{
+    public int Id { get; set; }
+    public DateTime Date { get; set; }
+    public int PatientId { get; set; }
+    public Patient Patient { get; set; }
+    public int PractitionerId { get; set; }
+    public Practitioner Practitioner { get; set; }
+}
