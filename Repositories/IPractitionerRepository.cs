c627b9fc6c2f4e7b0553af9993d96f1d6e652d38
kevin-trochon <157088235+Techktr@users.noreply.github.com>
2026-03-29 12:49:54 +1100

    feat: Ajout du repository pour Practitionner.

--- Repositories/IPractitionerRepository.cs
+++ Repositories/IPractitionerRepository.cs
@@ -1,6 +1,16 @@
-﻿namespace MedicoAPI.Repositories;
+﻿using MedicoAPI.Models;
+
+namespace MedicoAPI.Repositories;
 
 public interface IPractitionerRepository
 {
-    
+    List<Practitioner> GetPractitioners();
+
+    Practitioner? GetPractitionerById(int id);
+
+    Practitioner createPractitioner(Practitioner practitioner);
+
+    Practitioner? updatePractitioner(Practitioner practitioner);
+
+    void DeletePractitioner(int id);
 }
--- Repositories/PractitionerRepository.cs
+++ Repositories/PractitionerRepository.cs
@@ -1,6 +1,56 @@
-﻿namespace MedicoAPI.Repositories;
+﻿using MedicoAPI.Data;
+using MedicoAPI.Models;
 
-public class PractitionerRepository
+namespace MedicoAPI.Repositories;
+
+public class PractitionerRepository: IPractitionerRepository
 {
-    
+    private MedicoDbContext _context;
+
+    public PractitionerRepository(MedicoDbContext context)
+    {
+        _context = context;
+    }
+
+    public List<Practitioner> GetPractitioners()
+    {
+        return _context.Practitioners.ToList();
+    }
+
+    public Practitioner? GetPractitionerById(int id)
+    {
+       return _context.Practitioners.Find(id);
+    }
+
+    public Practitioner createPractitioner(Practitioner practitioner)
+    {
+        _context.Practitioners.Add(practitioner);
+        _context.SaveChanges();
+        return practitioner;
+    }
+
+    public Practitioner? updatePractitioner(Practitioner practitioner)
+    {
+        Practitioner? practitionerToUpdate = _context.Practitioners.Find(practitioner.Id);
+        if (practitionerToUpdate == null)
+        {
+            throw new Exception("Practitioner not exists");
+        }
+        practitionerToUpdate.Address = practitioner.Address;
+        practitionerToUpdate.Speciality = practitioner.Speciality;
+        _context.Practitioners.Update(practitionerToUpdate);
+        _context.SaveChanges();
+        return practitionerToUpdate;
+    }
+
+    public void DeletePractitioner(int id)
+    {
+        Practitioner? practitionerToDelete = _context.Practitioners.Find(id);
+        if (practitionerToDelete == null)
+        {
+            throw new Exception("Practitioner not exists");
+        }
+        _context.Practitioners.Remove(practitionerToDelete);
+        _context.SaveChanges();
+    }
 }
