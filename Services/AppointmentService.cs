7e881d594f751431c459fcb34345015c8293943a
kevin-trochon <157088235+Techktr@users.noreply.github.com>
2026-03-29 15:33:44 +1100

    feat: ajout du service rendez-vous.

---
+++ Services/AppointmentService.cs
@@ -0,0 +1,58 @@
+﻿using MedicoAPI.Models;
+using MedicoAPI.Repositories;
+using MedicoAPI.Services.DTOs;
+using MedicoAPI.Services.Mappers;
+
+namespace MedicoAPI.Services;
+
+public class AppointmentService: IAppointmentService
+{
+    private IAppointmentRepository _repository;
+    private AppointmentMapper _mapper;
+
+    public AppointmentService(AppointmentMapper mapper, IAppointmentRepository repository)
+    {
+        _mapper = mapper;
+        _repository = repository;
+    }
+
+    public List<AppointmentResponseDto> GetAppointments()
+    {
+        return _mapper.EntityToResponseDtos(_repository.GetAppointments());
+    }
+
+    public AppointmentResponseDto GetAppointmentById(int id)
+    {
+        Appointment? appointment = _repository.GetAppointmentById(id);
+        if (appointment == null)
+        {
+            throw new Exception("Appointment not found");
+        }
+        return _mapper.EntityToResponseDto(appointment) ?? throw new Exception("Appointment not found");
+    }
+
+    public AppointmentResponseDto CreateAppointment(AppointmentCreateDto patientCreateDto)
+    {
+        Appointment appointment = _repository.CreateAppointment(_mapper.EntityToCreateDto(patientCreateDto));
+        if (appointment == null)
+        {
+            throw new Exception("Appointment not created");
+        }
+        return _mapper.EntityToResponseDto(appointment);
+    }
+
+    public AppointmentResponseDto UpdateAppointment(AppointmentUpdateDto patientUpdateDto)
+    {
+        Appointment? appointment = _repository.UpdateAppointment(_mapper.EntityToUpdateDto(patientUpdateDto));
+        if (appointment == null)
+        {
+            throw new Exception("Appointment not found");
+        }
+        return  _mapper.EntityToResponseDto(appointment) ?? throw new Exception("Appointment not found");
+    }
+
+    public void DeleteAppointment(int id)
+    {
+        _repository.DeleteAppointment(id);
+    }
+}
---
+++ Services/IAppointmentService.cs
@@ -0,0 +1,12 @@
+﻿using MedicoAPI.Services.DTOs;
+
+namespace MedicoAPI.Services;
+
+public interface IAppointmentService
+{
+    List<AppointmentResponseDto> GetAppointments();
+    AppointmentResponseDto GetAppointmentById(int id);
+    AppointmentResponseDto CreateAppointment(AppointmentCreateDto patientCreateDto);
+    AppointmentResponseDto UpdateAppointment(AppointmentUpdateDto patientUpdateDto);
+    void DeleteAppointment(int id);
+}
