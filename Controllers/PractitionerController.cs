2d21ce80b9a44b14a8585a6ba1ec47eee3c722a3
kevin-trochon <157088235+Techktr@users.noreply.github.com>
2026-03-29 12:54:04 +1100

    feat: ajout du controller pour practitioner.

--- Controllers/PractitionerController.cs
+++ Controllers/PractitionerController.cs
@@ -1,6 +1,49 @@
-﻿namespace MedicoAPI.Controllers;
+﻿using MedicoAPI.Services;
+using MedicoAPI.Services.DTOs;
+using Microsoft.AspNetCore.Mvc;
 
-public class PractitionerController
+namespace MedicoAPI.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class PractitionerController: ControllerBase
 {
-    
+    private IPractitionerService _practitionerService;
+
+    public PractitionerController(IPractitionerService practitionerService)
+    {
+        _practitionerService = practitionerService;
+    }
+
+    [HttpGet]
+    public ActionResult<List<PractitionerResponseDto>> GetPractitioners()
+    {
+        return Ok(_practitionerService.GetPractitioners());
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<PractitionerResponseDto> GetPractitionerById(int id)
+    {
+        return Ok(_practitionerService.GetPractitionerById(id));
+    }
+
+    [HttpPost]
+    public IActionResult CreatePractitioner(PractitionerCreateDto patientCreateDto)
+    {
+        PractitionerResponseDto practitioner = _practitionerService.CreatePractitioner(patientCreateDto);
+        return Created($"api/practitioner/{practitioner.Id}", practitioner);
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult UpdatePractitioner(PractitionerUpdateDto patientUpdateDto)
+    {
+        return Ok(_practitionerService.UpdatePractitioner(patientUpdateDto));
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult DeletePractitioner(int id)
+    {
+        _practitionerService.DeletePractitioner(id);
+        return NoContent();
+    }
 }
